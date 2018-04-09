54cbf9bb95105833d647ad36b8a705c73c08666c
Eric Middleton <ericm@iastate.edu>
2018-04-09 02:08:36 -0500

    Added implementation of parallel merge sort

---
+++ merge_sort/main.c
@@ -0,0 +1,52 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <mpi.h> 
+
+#include "merge_sort.h"
+
+#define ARRAY_SIZE		1024
+
+void print_array(int* arr, size_t size);
+
+int main(void) {
+   int my_rank, comm_sz;
+
+   MPI_Init(NULL, NULL); 
+   MPI_Comm_size(MPI_COMM_WORLD, &comm_sz); 
+   MPI_Comm_rank(MPI_COMM_WORLD, &my_rank); 
+
+	int* arr;
+	if(my_rank == 0) {
+		arr = (int*)malloc(ARRAY_SIZE * sizeof(int));
+		int i;
+		for(i = 0; i < ARRAY_SIZE; ++i) {
+			arr[i] = rand() % 100;
+		}
+	}
+	
+	merge_sort(arr, ARRAY_SIZE, my_rank, comm_sz);
+	
+	if(my_rank == 0) {
+		if(validate(arr, ARRAY_SIZE)) {
+			printf("[Info] Validation successful!\n");
+		}
+		else {
+			printf("[Error] Validation not successful :(\n");
+			print_array(arr, ARRAY_SIZE);
+		}
+
+		free(arr);
+	}
+
+   MPI_Finalize();
+   return 0;
+}  /* main */
+
+void print_array(int* arr, size_t size) {
+	printf("\t");
+	int i;
+	for(i = 0; i < size; ++i) {
+		printf("%d ", arr[i]);
+	}
+	printf("\n");
+}
---
+++ merge_sort/merge_sort.h
@@ -0,0 +1,5 @@
+#pragma once
+
+#include <stddef.h>
+
+void merge_sort(int arr[], size_t size, int my_rank, int comm_sz);
